6de6ad87e98596aae6ae86d608e46da51a08348c
Theodore Ando <theodoreando99@gmail.com>
2018-03-16 13:16:46 -0400

    Added mini navbar and table and clickable star

--- rooms/views.py
+++ rooms/views.py
@@ -1,14 +1,17 @@
 from flask import render_template
+from pony.orm import db_session
 from pony.orm import select
 
 from rooms import app
 import rooms.dbmanager as dbm
 import rooms.conf as conf
 
+db = dbm.connect(conf.DB_NAME, conf.DB_TYPE)
+
 
 @app.route("/")
+@db_session
 def table():
-    db = dbm.connect(conf.DB_NAME, conf.DB_TYPE)
     query = select(r for r in db.Room)
     room_data = [r.to_dict() for r in query]
     return render_template('table.html', room_data=room_data)
