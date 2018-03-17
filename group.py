1592a51312f9cb98f4b6d4f410d21cf2a9987286
Theodore Ando <theodoreando99@gmail.com>
2018-03-17 14:13:40 -0400

    Add cautionary comment
    Warnings about things that appear useless but will break things if
    removed because flask app routes are not caught by IDE.

---
+++ group.py
@@ -0,0 +1,18 @@
+from flask import logging
+from pony.orm import db_session, select
+
+from rooms import app, cas
+import rooms.conf as conf
+import rooms.dbmanager as dbm
+
+db = dbm.connect(conf.DB_NAME, conf.DB_TYPE)
+
+logger = logging.getLogger(conf.LOGGER)
+
+
+@app.route("/request_group", methods=["POST"])
+@cas.authenticated()
+@db_session
+def request_group():
+    # TODO: make new entry in group request table
+    raise NotImplementedError()
--- rooms/views.py
+++ rooms/views.py
@@ -6,8 +6,11 @@ from rooms import app
 import rooms.dbmanager as dbm
 import rooms.conf as conf
 
+# DON'T REMOVE THESE!!!  They appear unused but need them for flask app
+# routings.
 import rooms.query
 import rooms.favorite
+import rooms.group
 
 db = dbm.connect(conf.DB_NAME, conf.DB_TYPE)
 
