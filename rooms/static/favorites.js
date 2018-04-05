8de760a5e800130b652c46399775a637d98f0d9f
Eitan <zlatineitan@gmail.com>
2018-04-04 22:34:53 -0400

    Coloring Stars, and messing with the allignment of the table. Got rid of the option to change the table length. Made it so you can click anywhere in the stars box and it will still favorite. Removed ability to order the table based on the favorite collumn.

--- rooms/static/favorites.js
+++ rooms/static/favorites.js
@@ -1,15 +1,33 @@
 function favorite(id) {
-    var elem = $("#" + id);
+    console.log('hi');
+    var elem = $("#" + id + "star");
     var filled = elem.attr("data-prefix") === "fas";
-    if (filled) elem.attr("data-prefix", "far");
-    else elem.attr("data-prefix", "fas");
+    if (filled) {
+        elem.attr("data-prefix", "far");
+    }
+    else {
+        elem.attr("data-prefix", "fas");
+    }
+
 }
 
 $(document).ready(function () {
             $('#roomsTable').DataTable({
                 pageResize: true,
                 searching: false,
+                lengthChange: true,
+                "columns": [ //Don't allow them to order based on the favorite star
+                    { "className": "dt-center", "orderable": false },
+                    { "className": "dt-center"},
+                    { "className": "dt-center"},
+                    { "className": "dt-center"},
+                    { "className": "dt-center"},
+                    { "className": "dt-center"},
+                    { "className": "dt-center", "width": "12%"},
+                    { "className": "dt-center", "width": "12%"},
+                    { "className": "dt-center", "width": "10%"
+                ],
                 colReorder: true,
-				lengthChange: true
+                order: [[ 1, 'asc' ]]  //Starting order is collumn 1, which is college, in ascending order
             });
 });
