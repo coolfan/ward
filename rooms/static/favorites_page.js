40b34648cbb293616629e8e3ad90b5b90ccfb53e
Yanjun Yang <yanjun.yang@princeton.edu>
2018-04-06 06:12:14 -0400

    things are more modular and less hacky, now displays some useful data

--- rooms/static/favorites_page.js
+++ rooms/static/favorites_page.js
@@ -1,78 +1,128 @@
-var card_queue = []
-
-function display_bigcard(s) {
-	if (typeof display_bigcard.disp1 === "undefined") {
-		display_bigcard.disp1 = true
-	}
-
-	if (display_bigcard.disp1) {
-		$("#bigcard1_body").text(s)
-	} else {
-		$("#bigcard2_body").text(s)
-	}
-
-	display_bigcard.disp1 = !display_bigcard.disp1
+var card_mgr = {
+	card_queue: [],
+	bigcard_arr: [null, null],
+	bigcard_disp_arr: [null, null],
+	cur_bigcard1: true
 }
 
-function undisplay_bigcard(s) {
-	if ($("#bigcard1_body").text() === s) {
-		$("#bigcard1_body").text("")
-		display_bigcard.disp1 = true
-	}
+function to_header(val) {
+	return val.roomnum + " " + val.building
+}
 
-	if ($("#bigcard2_body").text() === s) {
-		$("#bigcard2_body").text("")
-		display_bigcard.disp1 = false
+function build_bigcard_inner(val) {
+	var header = $("<p>").addClass("col").addClass("col-sm-3")
+	header.text(to_header(val))
+
+	var occupancy = $("<p>").addClass("col").addClass("col-sm-3")
+	occupancy.text("Occupancy: " + val.occupancy)
+
+	var floor = $("<p>").addClass("col").addClass("col-sm-3")
+	floor.text("Floor: " + val.floor)
+
+	var subfree = $("<p>").addClass("col").addClass("col-sm-3")
+	subfree.text("Sub-Free: " + (val.subfree ? "Yes" : "No"))
+
+	return [header, occupancy, floor, subfree]
+}
+
+function get_dom_id(val) {
+	return "card" + val.id
+}
+
+function build_bigcard_inner(val) {
+	var header = $("<p>").addClass("col").addClass("col-sm-3")
+	header.text(to_header(val))
+
+	var occupancy = $("<p>").addClass("col").addClass("col-sm-3")
+	occupancy.text("Occupancy: " + val.occupancy)
+
+	var floor = $("<p>").addClass("col").addClass("col-sm-3")
+	floor.text("Floor: " + val.floor)
+
+	var subfree = $("<p>").addClass("col").addClass("col-sm-3")
+	subfree.text("Sub-Free: " + (val.subfree ? "Yes" : "No"))
+
+	return [header, occupancy, floor, subfree]
+}
+
+function display_bigcard(val) {
+	var index = card_mgr.cur_bigcard1 ? 0 : 1
+	
+	card_mgr.bigcard_arr[index].empty()
+	$.each(build_bigcard_inner(val), function(i, elem) {
+		card_mgr.bigcard_arr[index].append(elem)
+	})
+	card_mgr.bigcard_disp_arr[index] = val
+	
+	card_mgr.cur_bigcard1 = !card_mgr.cur_bigcard1;
+
+	card_mgr.card_queue.push(val)
+	if (card_mgr.card_queue.length > 2) {
+		var ret = card_mgr.card_queue.shift()
+		$("#" + get_dom_id(ret)).click()
 	}
 }
 
-function get_card(s, id) {
+function undisplay_bigcard(val) {
+	if (card_mgr.bigcard_disp_arr[0] !== null && card_mgr.bigcard_disp_arr[0].id === val.id) {
+		card_mgr.bigcard_arr[0].empty()
+		card_mgr.bigcard_disp_arr[0] = null
+		card_mgr.cur_bigcard1 = true
+	}
+
+	if (card_mgr.bigcard_disp_arr[1] !== null && card_mgr.bigcard_disp_arr[1].id === val.id) {
+		card_mgr.bigcard_arr[1].empty()
+		card_mgr.bigcard_disp_arr[1] = null
+		card_mgr.cur_bigcard1 = false
+	}
+
+	for (var i = 0; i < card_mgr.card_queue.length; i++) {
+		if (card_mgr.card_queue[i].id === val.id) {
+			card_mgr.card_queue.splice(i, 1)
+			break;
+		}
+	}
+}
+
+function get_card(val) {
 	var card = $("<div>").addClass("card")
 	var container_fluid = $("<div>").addClass("container-fluid")
 	var card_body = $("<div>").addClass("card-body")
 	var row = $("<div>").addClass("row")
 	var col = $("<div>").addClass("col-sm-12")
 	var text = $("<p>").addClass("card-text")
-	text.append(s)
+	
+	text.append(to_header(val))
 	col.append(text)
 	row.append(col)
 	card_body.append(row)
 	container_fluid.append(card_body)
 	card.append(container_fluid)
-
+	
 	card.css("margin-bottom", "10px")
-	card.bool_filled = false
+	val.bool_filled = false
 
 	card.click(function() {
-		if (card.bool_filled) {
-			card.css("background-color", "white")
-			undisplay_bigcard(s)
-			for (var i = 0; i < card_queue.length; i++) {
-				if (card_queue[i] === card) {
-					card_queue.splice(i, 1)
-					break;
-				}
-			}
+		if (val.bool_filled) {
+			card.css("backgroundColor", "white")
+			undisplay_bigcard(val)
 		} else {
-			card.css("background-color", "#f2f5ff")
-			display_bigcard(s)
-			card_queue.push(card)
-			if (card_queue.length > 2) {
-				var ret = card_queue.shift()
-				ret.click()
-			}
+			card.css("backgroundColor", "#f2f5ff")
+			display_bigcard(val)
 		}
-		card.bool_filled = !card.bool_filled
+		val.bool_filled = !val.bool_filled
 	});
-
+	card.attr("id", get_dom_id(val))
 	return card
 }
 
 $("#cards").ready(function() {
+	card_mgr.bigcard_arr = [$("#bigcard1_body"), $("#bigcard2_body")]
 	$.getJSON("/favorites", function(data) {
 		console.log(data)
 		$.each(data, function(i, val) {
-			$("#cards").append(get_card(val.building + " " + val.roomnum, val.id))
+			var card = get_card(val)
+			$("#cards").append(get_card(val))
 		});
 	});
 });
