0ed66b252897335cecb02d9e4af4f2e16c302c02
Eitan <zlatineitan@gmail.com>
2018-04-07 18:10:50 -0400

    Misc thingies

--- rooms/static/big_card.js
+++ rooms/static/big_card.js
@@ -1,5 +1,5 @@
-function get_big_card(){
-    let medium_card = get_medium_card();
+function get_big_card(room){
+    let medium_card = get_medium_card(room);
     let small_card = medium_card[0];
     medium_card = medium_card[1];
 
@@ -7,11 +7,14 @@ function get_big_card(){
         <div class = "collapse row Big_collapse">
             <div class = "col-sm-12 padding-0">
                 <div class = "container-fluid padding-0">
+                    <p>hi</p>
                 <\div>
             <\div>
         <\div>
     `);
 
+    let big_collapse_div = $(html_card).find(".Big_collapse");
 
+    big_collapse_div.attr('id', room['id'] + 'big_card');
     return [small_card,medium_card,html_card];
 }
