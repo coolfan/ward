00cebd42b496ac6fe25cba5b88ecff6425352729
Yanjun Yang <yanjun.yang@princeton.edu>
2018-04-07 18:30:17 -0400

    should be able to display who is in your group now

--- rooms/static/account_page.js
+++ rooms/static/account_page.js
@@ -1,4 +1,4 @@
-function get_card(val) {
+function get_pending_card(val) {
 	var card = $("<div>").addClass("card")
 	var container_fluid = $("<div>").addClass("container-fluid")
 	var card_body = $("<div>").addClass("card-body")
@@ -16,7 +16,8 @@ function get_card(val) {
 	check.append($("<i>").addClass("fa").addClass("fa-check"))
 	check.click(function() {
 		$.get("/approve_group", {request_id: val.id, action: "accept"}, function(data) {
-			alert(val.from_user + " accepted successfully!")
+			update_group()
+			update_pending()
 		})
 	})
 
@@ -24,7 +25,8 @@ function get_card(val) {
 	cross.append($("<i>").addClass("fa").addClass("fa-close"))
 	cross.click(function() {
 		$.get("/approve_group", {request_id: val.id, action: "reject"}, function(data) {
-			alert(val.from_user + " rejected successfully!")
+			update_group()
+			update_pending()
 		})
 	})
 
@@ -50,14 +52,50 @@ function get_card(val) {
 	return card
 }
 
-$(document).ready(function() {
+function get_group_card(val) {
+	var card = $("<div>").addClass("card")
+	var container_fluid = $("<div>").addClass("container-fluid")
+	var card_body = $("<div>").addClass("card-body")
+	var row1 = $("<div>").addClass("row")
+	var col11 = $("<div>").addClass("col-sm-12")
+	var header = $("<p>").addClass("card-text")
+
+	header.text(val.netid)
+
+	col11.append(header)
+
+	row1.append(col11)
+	card_body.append(row1)
+	container_fluid.append(card_body)
+	card.append(container_fluid)
+
+	card.css("margin-bottom", "10px")
+
+	return card
+}
+
+function update_pending() {
 	$.get("/pending_requests", function(data) {
 		$.each(data, function(i, val) {
-			var card = get_card(val)
+			var card = get_pending_card(val)
 			$("#pending").append(card)
-			console.log(val)
 		})
 	})
+}
+
+function update_group() {
+	$.get("/my_group", function(data) {
+		$.each(data, function(i, val) {
+			var card = get_pending_card(val)
+			$("#members").append(card)
+		})
+	})
+}
+
+$(document).ready(function() {
+	
+	update_group()
+	update_pending()
 
 	$("#add_member_form").submit(function(e) {
 		e.preventDefault()
