dbb9a47cb46e2428e65602151de552224643c953
Yanjun Yang <yanjun.yang@princeton.edu>
2018-04-10 00:50:18 -0400

    reviews page now properly uploads files

--- rooms/static/reviews_page.js
+++ rooms/static/reviews_page.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 	navbar_set("#nav_reviews")
+	/*$("#review").submit(function(e) {
+		e.preventDefault()
+		var roomid = $("#roomid").val()
+		var rating = $("#rating").val()
+		var text = $("#text").val()
+		var file = $("#pictures").val()
+		var filename = $("#pictures")[0].value
+		console.log(filename)
+		$.post("/review", {roomid: roomid, rating: rating, text: text, pictures: file}, function() {
+			$("#review")[0].reset()
+		})
+	})*/
 })
