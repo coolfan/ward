16aba4b2a7a53c7966aab8fed5395008e82eeb4c
Theodore Ando <theodoreando99@gmail.com>
2018-04-11 23:00:33 -0400

    Updated database to dynamically change between dev and production
    Will use SQLite3 for dev and MySQL for prod

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,20 +1,8 @@
 import os,sys
 INTERP = "/n/fs/rooms/venv/bin/python"
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+os.environ['PRODUCTION'] = True  # enabled when the site is running in production!
 
 sys.path.append("/n/fs/rooms/COS333")
 
 from rooms import app as application
-
-# import pwd
-# import grp
-#
-# def get_username():
-#     return pwd.getpwuid(os.getuid()).pw_name
-#
-# def get_groups():
-#     return grp.getgrgid(os.getgid()).gr_name
-#
-# def application(env, start_response):
-#     start_response('200 OK', [('Content-Type','text/html')])
-#     return ["Hello World!"]
