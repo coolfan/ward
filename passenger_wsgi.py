227f9b2abf8aa49c477e56ab38505bab0c7fd7ac
Jacob Edelman <edelman.jd@gmail.com>
2018-04-12 23:41:27 -0400

    prettyfied *.py files

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,12 @@
-import os,sys
+import os
+import sys
+from rooms import app as application  # not sure why this is here tbh?!
+
 INTERP = "/n/fs/rooms/venv/bin/python"
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-os.environ['ENV'] = "PRODUCTION"  # enabled when the site is running in production!
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+# enabled when the site is running in production!
+os.environ['ENV'] = "PRODUCTION"
 
 sys.path.append("/n/fs/rooms/COS333")
-
-from rooms import app as application
