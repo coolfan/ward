2f27a3bdb5a00e1329983da70ec00a23eb6a9ef2
Jacob Edelman <edelman.jd@gmail.com>
2018-04-12 22:47:57 -0400

    fixing the json encoder and improving it

--- rooms/flask_extensions.py
+++ rooms/flask_extensions.py
@@ -3,6 +3,7 @@ from rooms import cas, conf
 from rooms import dbmanager as dbm
 from functools import wraps
 from pony.orm.core import Query as PonyQuery
+from pony.orm.core import Entity as PonyEntity
 from flask.json import JSONEncoder
 
 class AuthBlueprint(Blueprint):
@@ -25,16 +26,22 @@ class AuthBlueprint(Blueprint):
 
 
 class ExtendedJSONEncoder(JSONEncoder):
-    """
-    Extends the flask json encoder to make it work on our database
-    objects, and pony queries, when need be.
-    """
-    @dbm.use_app_db
-    def default(db, self, o): # correct place?
+    def default(self, o):
+        """
+        Extends the flask json encoder to make it work on our database
+        objects, and pony queries, when need be.
+        """
         if isinstance(o, PonyQuery):
             return list(o)
-        elif isinstance(o, db.GroupRequest):
-            req = o.to_dict()
-            req["from_user"] = db.User[req["from_user"]].netid
-            return req # or use self??
+        elif isinstance(o, PonyEntity):
+            db = dbm.get_app_db()
+
+            # assert it is from the current database
+            assert isinstance(o, db.Entity)
+
+            if isinstance(o, db.GroupRequest):
+                req = o.to_dict()
+                req["from_user"] = db.User[req["from_user"]].netid
+                return req
+            return o.to_dict()
         return JSONEncoder.default(self, o)
