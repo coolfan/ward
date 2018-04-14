7c71e4829f651d7df68602231e614a7a0d7c2067
Eitan <zlatineitan@gmail.com>
2018-04-13 20:38:38 -0400

    Basic search functionality actually works now

--- rooms/static/javascript/form_manager.js
+++ rooms/static/javascript/form_manager.js
@@ -1,23 +1,88 @@
-function setup_form(){
-    $("#search_form").submit(function(e){
-        e.preventDefault();
+let buildings = {}; //A mapping between colleges and buildings
 
-        let draw = $("#draw_type_select").val();
-        let subfree = $("#check_subfree").val();
+function setup_form() {
+    let search_form = $("#search_form");
 
-        let object = {
-            college:draw
-        };
-        $.get("/query",object,function(data){
-            rooms = data;
-            display_rooms();
-        })
-    };
+    let draw_type_select = $("#draw_type_select");
+    let check_subfree = $("#check_subfree");
+    let building_select = $("#building_select");
+    let occupancy_input = $("#occupancy_input");
+    let num_rooms_input = $("#num_rooms_input");
+    let floor_input = $("#floor_input");
 
-    $("#draw_type_select").click(function() {
-        if ($("#draw_type_select").val() !== "") {
+    // //Get neccessary data
+    // $.get("/buildings", function (data) {
+    //     buildings = data;
+    // });
+
+    //Fill in data
+    for (let college in buildings) {
+        let option = $(`<option></option>`);
+        option.attr("value", college);
+        option.text(college);
+
+        draw_type_select.appendChild(option);
+    }
+
+
+    //Change elements behavior
+    draw_type_select.click(function () {
+        if (draw_type_select.val() !== "") {
             $("#draw_type_fake_option").remove();
         }
     });
 
+    draw_type_select.change(function () {
+        college = draw_type_select.val();
+        for (let building in buildings[college]) {
+            let option = $(`<option></option>`);
+            option.attr("value",building);
+            option.text(building);
+        }
+    });
+
+    search_form.submit(function (e) {
+        e.preventDefault();
+
+        let college = draw_type_select.val();
+        let subfree = check_subfree.val() === "off";
+        let building = building_select.val();
+        let occupancy = occupancy_input.val();
+        let num_rooms = num_rooms_input.val();
+        let floor = floor_input.val();
+
+
+        let room_query = {};
+
+        if (college !== ""){
+            room_query["college"] = college
+        }
+
+        if (subfree){
+            room_query["subfree"] = subfree
+        }
+
+        if (building !== ""){
+            room_query["building"] = building
+        }
+
+        if (occupancy !== ""){
+            room_query["occupancy"] = occupancy
+        }
+
+        if (num_rooms !== ""){
+            room_query["numrooms"] = num_rooms
+        }
+
+        if (floor !== ""){
+            room_query["floor"] = floor
+        }
+
+
+        console.log(floor)
+        $.get("/query", room_query, function (data) {
+            rooms = data;
+            display_rooms();
+        })
+    })
 }
