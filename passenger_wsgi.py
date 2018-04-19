3a4610d43545adf511df709fb34cc739b51e43ba
Theodore Ando <theodoreando99@gmail.com>
2018-04-18 22:32:32 -0400

    Updating passenger wsgi to properly source venv

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,19 @@
-import os,sys
+import os
+import sys
+
+# Make sure to use the right version of python as set in venv
 INTERP = "/n/fs/rooms/venv/bin/python"
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-os.environ['ENV'] = "PRODUCTION"  # enabled when the site is running in production!
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+# Use the virtual environment packages and stuff
+activate_this = '/n/fs/rooms/venv/bin/activate_this.py'
+exec(open(activate_this).read(), dict(__file__=activate_this))
+
+# Keep this line! Passenger Phusion requires a callable 'application'
+from rooms import app as application
+
+# enabled when the site is running in production
+os.environ['ENV'] = "PRODUCTION"
 
 sys.path.append("/n/fs/rooms/COS333")
-
-from rooms import app as application
