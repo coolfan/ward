8d05c0fd332b5b05fcf05e41ca95c85490485db5
Yanjun Yang <yanjun.yang@princeton.edu>
2018-04-19 16:52:56 -0400

    notification for review page changed to label, also added dropdown menu for building selection

--- rooms/static/javascript/reviews_page.js
+++ rooms/static/javascript/reviews_page.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 	navbar_set("#nav_reviews")
+
+	$.get("/buildings", function(data) {
+		$.each(data, function(i, val) {
+			var option = $("<option>").text(val)
+			$("#building").append(option)
+		})
+	})
 	
 	$("#button").click(function() {
 		$.get("/query", {building: $("#building").val(), roomnum: $("#roomnum").val()}, function(data) {
@@ -10,15 +17,17 @@ $(document).ready(function() {
 				if (rating.length > 0) {
 					$("#rating").val(rating.attr("id").slice(3))
 				} else {
-					alert("Please select a rating.")
+					$("#notif").text("Please select a rating.")
 				}
 				
 				$("#review").submit()
+				$("#notif").text("Review for " + $("#building").val() + " " + $("#roomnum").val() + " has been submitted!")
 				$("#review")[0].reset()
 				$("input[name=ratings-choice]:checked").removeAttr("checked")
 				$("label").removeClass("active")
+				
 			} else {
-				alert("Please enter a valid room.")
+				$("#notif").text("Please enter a valid room.")
 			}
 		})
 	})
