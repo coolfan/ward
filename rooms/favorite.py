41f915fc004fefe3e68a7f83ea7030f108ffca5a
Theodore Ando <theodoreando99@gmail.com>
2018-05-01 20:24:17 -0400

    Updating favorites API to fit with groups
    Most favorite related function now take an optional groupid argument.

--- rooms/favorite.py
+++ rooms/favorite.py
@@ -2,40 +2,60 @@ from flask import request, logging, jsonify, session, Response, Blueprint, \
     abort, current_app
 from pony.orm import db_session, select
 
-from rooms import app, cas, conf
+from rooms import cas, conf
+from rooms.flask_extensions import AuthBlueprint
 import rooms.dbmanager as dbm
 
-blueprint = Blueprint("favorite", __name__)
+blueprint = AuthBlueprint("favorite", __name__)
 
 logger = logging.getLogger(conf.LOGGER)
 
 
-@blueprint.route("/favorite", methods=["GET"])
-@cas.authenticated
-@dbm.use_app_db
-def favorite(db) -> Response:
+def _verify_args(request_args, user, db):
+    """
+    Verify the args to a favorite function - checks valid roomid, groupid.
+    :returns: if valid, returns the room, group
+    """
+    if "roomid" not in request_args:
+        return False, Response("Missing roomid", 400), None
+    room_id = request_args["roomid"]
+
+    if not db.Room.exists(id=room_id):
+        return False, Response("Invalid roomid", 422), None
+    room = db.Room[room_id]
+
+    group = None
+    if "groupid" in request_args:
+        group_id = request_args["groupid"]
+        if not db.Group.exists(id=group_id):
+            return False, Response("Invalid groupid", 422), None
+        group = db.Group[group_id]
+
+        if user not in group.members:
+            return False, Response("You are not in this group", 403), None
+
+    return True, room, group
+
+
+
+
+@blueprint.auth_route("/favorite", methods=["GET"])
+def favorite(user, db) -> Response:
     """
     Adds a room to favorites list of group to which the currently logged in
     user belongs.  Expects parameters in url params:
         - roomid: id of the room to add to favorites list
     :return: Response(200) if successful
     """
-    if "roomid" not in request.args:
-        return Response("Missing roomid", 400)
-    roomid = request.args.get("roomid")
-    current_app.logger.debug(f"favorite received room id {roomid}")
 
-    if not db.Room.exists(id=roomid):
-        return Response("Invalid roomid", 422)
-    room = db.Room[roomid]
-
-    netid = cas.netid()
-
-    user = db.User.get_or_create(netid=netid)
-
-    # User must have a personal favorites list!
-    # TODO: generalize this process
+    # falg indicates validity.  Other holds error response, or tuple room, group
+    flag, *other = _verify_args(request.args, user, db)
+    if not flag:
+        return other[0]
+    room, group = other
     ranked_room_list = user.getfavoritelist()
+    if group is not None:
+        ranked_room_list = group.getfavoritelist()
 
     if room in ranked_room_list:
         current_app.logger.debug("Favoriting an already favorite room")
@@ -49,28 +69,22 @@ def favorite(db) -> Response:
     return jsonify({'success': True})
 
 
-@blueprint.route("/unfavorite", methods=["GET"])
-@cas.authenticated
-@dbm.use_app_db
-def unfavorite(db):
+@blueprint.auth_route("/unfavorite", methods=["GET"])
+def unfavorite(user, db):
     """
     Removes a room from favorites list of group to which the currently
     logged in user belongs.  Expects parameters in form data:
         - roomid: id of the room to remove from favorite list
     :return:
     """
-    if "roomid" not in request.args:
-        return Response("Missing roomid", 400)
-    roomid = request.args.get("roomid")
-
-    if not db.Room.exists(id=roomid):
-        return Response("Invalid roomid", 422)
-    room = db.Room[roomid]
-
-    netid = cas.netid()
-    user = db.User.get_or_create(netid=netid)
-
+    flag, *other = _verify_args(request.args, user, db)
+    if not flag:
+        return other[0]
+    room, group = other
     ranked_room_list = user.getfavoritelist()
+    if group is not None:
+        ranked_room_list = group.getfavoritelist()
+
     if room in ranked_room_list:
         rr = ranked_room_list.get_by_room(room)
         current_app.logger.debug(f"Deleting ranked room {rr}")
@@ -85,23 +99,22 @@ def unfavorite(db):
     return jsonify({'success': True})
 
 
-@blueprint.route("/reorder_favorites", methods=["POST"])
-@cas.authenticated
-@dbm.use_app_db
-def reorder_favorites(db):
-    netid = cas.netid()
-    user = db.User.get_or_create(netid=netid)
+@blueprint.auth_route("/reorder_favorites", methods=["POST"])
+def reorder_favorites(user, db):
+    flag, *other = _verify_args(request.args, user, db)
+    if not flag:
+        return other[0]
+    room, group = other
+    ranked_room_list = user.getfavoritelist()
+    if group is not None:
+        ranked_room_list = group.getfavoritelist()
 
     favoriteid_list = request.get_json()
-    real_favoriteid_list = select(room for room in db.FavoriteRoom if
-                                room.group == user.group)
+    real_favoriteid_list = {rr.id for rr in ranked_room_list.ranked_rooms}
     if set(favoriteid_list) != set(real_favoriteid_list):
         abort(400)
 
-    faverooms = [db.FavoriteRoom.get(id=fid) for fid in favoriteid_list]
-    for faveroom in faverooms:
-        if faveroom is None or faveroom.group != user.group:
-            abort(403)
+    faverooms = [db.RankedRoom.get(id=fid) for fid in favoriteid_list]
 
     for newrank, faveroom in enumerate(faverooms): # this should fix things??
         faveroom.rank = newrank
@@ -109,16 +122,12 @@ def reorder_favorites(db):
     return jsonify({"success": True})
 
 
-@blueprint.route("/favorites", methods=["GET"])
-@cas.authenticated
-@dbm.use_app_db
-def favorites(db):
+@blueprint.auth_route("/favorites", methods=["GET"])
+def favorites(user, db):
     """
     Return a list of the rooms in your favorites list, sorted by rank
     :return:
     """
-    netid = cas.netid()
-    user = db.User.get_or_create(netid=netid)
     groups = user.groups
 
     group_id = request.args.get("id")
