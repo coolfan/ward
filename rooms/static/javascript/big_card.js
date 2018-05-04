439cb8ebd77bc99314d768afa9c0419ca4896450
Yanjun Yang <yanjun.yang@princeton.edu>
2018-05-04 12:40:47 -0400

    fixed up big card display and added likelihood string to big card

--- rooms/static/javascript/big_card.js
+++ rooms/static/javascript/big_card.js
@@ -9,6 +9,21 @@ function build_bigcard_inner(val) {
 	var drawtype = $("<h5>").addClass("row")
 	drawtype.text(val.college)
 
+	var likelihood = val.likelihood;
+    
+    if (likelihood <= 100 && likelihood >= 66) {
+		likelihood = "Likely"
+    } else if (likelihood < 66 && likelihood >= 33){
+        likelihood = "Maybe";
+    } else if (likelihood < 33 && likelihood >= 10){
+        likelihood = "Unlikely";
+    } else if (likelihood < 10 && likelihood >= 0){
+        likelihood = "Doomed";
+    }
+
+	var likelihood2 = $("<p>").addClass("row")
+	likelihood2.text("Likelihood Estimate: " + likelihood)
+
 	var numrooms = $("<p>").addClass("row")
 	numrooms.text("Number of Rooms: " + val.numrooms)
 
@@ -21,11 +36,11 @@ function build_bigcard_inner(val) {
 	var subfree = $("<p>").addClass("row")
 	subfree.text("Sub-Free: " + (val.subfree ? "Yes" : "No"))
 
-	return [header, drawtype, numrooms, occupancy, floor, subfree]
+	return [header, drawtype, likelihood2, numrooms, occupancy, floor, subfree]
 }
 
 function get_big_card(room, reviews){
-    var div = $("<div>")
+    var div = $("<div>").addClass("w-100")
 	$.each(build_bigcard_inner(room), function(i, val) {
 		div.append(val)
 	})
