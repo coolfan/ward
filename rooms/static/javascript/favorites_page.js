7e899d4c1e8c0dc0d3a231b65594573dcde4f488
Yanjun Yang <yanjun.yang@princeton.edu>
2018-05-06 19:39:07 -0400

    changed height of certain objects to prevent scrolling

--- rooms/static/javascript/favorites_page.js
+++ rooms/static/javascript/favorites_page.js
@@ -494,6 +494,7 @@ $(document).ready(function() {
 	})
 	
 	navbar_set("#nav_favorites")
+	console.log(RegExp('multipage', 'gi'))
 	if (RegExp('multipage', 'gi').test(window.location.search)) {
 		introJs().start();
 	}
