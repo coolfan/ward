f7d027e6ba0417c0a2ade3df721f5ad60166e5b6
Yanjun Yang <yanjun.yang@princeton.edu>
2018-05-11 10:41:39 -0400

    fixed bug where network delay could break accounts page

--- rooms/static/javascript/account_page.js
+++ rooms/static/javascript/account_page.js
@@ -1,57 +1,3 @@
-/*function get_pending_card(val) {
-	var card = $("<div>").addClass("card")
-	var container_fluid = $("<div>").addClass("container-fluid")
-	var card_body = $("<div>").addClass("card-body")
-	var row1 = $("<div>").addClass("row")
-	var row2 = $("<div>").addClass("row")
-	var col11 = $("<div>").addClass("col-sm-10")
-	var col12 = $("<div>").addClass("col-sm-1")
-	var col13 = $("<div>").addClass("col-sm-1")
-	var col21 = $("<div>").addClass("col-sm-12")
-	var header = $("<p>").addClass("card-text")
-	var text = $("<p>").addClass("card-text")
-
-
-	var check = $("<a>")
-	check.append($("<i>").addClass("fa").addClass("fa-check"))
-	check.click(function() {
-		$.get("/approve_group", {request_id: val.id, action: "accept"}, function(data) {
-			update_group()
-			update_pending()
-		})
-	})
-
-	var cross = $("<a>")
-	cross.append($("<i>").addClass("fa").addClass("fa-close"))
-	cross.click(function() {
-		$.get("/approve_group", {request_id: val.id, action: "reject"}, function(data) {
-			update_group()
-			update_pending()
-		})
-	})
-
-	header.text(val.from_user)
-	text.text(val.message)
-
-	col11.append(header)
-	col12.append(check)
-	col13.append(cross)
-	col21.append(text)
-
-	row1.append(col11)
-	row1.append(col12)
-	row1.append(col13)
-	row2.append(col21)
-	card_body.append(row1)
-	card_body.append(row2)
-	container_fluid.append(card_body)
-	card.append(container_fluid)
-
-	card.css("margin-bottom", "10px")
-
-	return card
-}*/
-
 function get_group_card(val) {
 	var card = $("<div>").addClass("card selectable-card")
 	var container_fluid = $("<div>").addClass("container-fluid")
@@ -74,7 +20,6 @@ function get_group_card(val) {
 
 	card.click(function() {
 		update_members(val.members)
-		// console.log(val.drawtime);
 		update_addinfo(val.drawtime)
 		update_selected(val.id)
 	})
@@ -104,19 +49,9 @@ function get_member_card(val) {
 	return card
 }
 
-/*function update_pending() {
-	$("#pending").empty()
-	$.get("/pending_requests", function(data) {
-		$.each(data, function(i, val) {
-			var card = get_pending_card(val)
-			$("#pending").append(card)
-		})
-	})
-}*/
-
 function update_groups() {
-	$("#groups").empty()
 	$.get("/my_groups", function(data) {
+		$("#groups").empty()
 		$.each(data, function(i, val) {
 			var card = get_group_card(val)
 			$("#groups").append(card)
@@ -125,8 +60,8 @@ function update_groups() {
 }
 
 function update_members(data) {
-	$("#members").empty()
 	$.each(data, function(i, val) {
+		$("#members").empty()
 		var card = get_member_card(val)
 		$("#members").append(card)
 	})
@@ -152,20 +87,6 @@ function update_selected(id) {
 }
 
 $(document).ready(function() {
-	
-	//update_members()
-	//update_pending()
 	update_groups()
-
-/*
-	$("#add_member_form").submit(function(e) {
-		e.preventDefault()
-		var netid = $("#add_member").val()
-		$.get("/request_group", {other_netid: netid}, function() {
-			$("#notif").text("Request to " + netid + " has been sent!")
-		})
-		$("#add_member").val("")
-	})
-*/
 	navbar_set("#nav_account")
 })
