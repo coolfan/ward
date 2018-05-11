6d05ef905d8da2cfb6de27c8a13f52d063b4cdb9
Yanjun Yang <yanjun.yang@princeton.edu>
2018-05-10 20:01:14 -0400

    changed modal size to regular

--- rooms/static/javascript/data_table.js
+++ rooms/static/javascript/data_table.js
@@ -276,7 +276,7 @@ function add_modal(row,room,index){
                     $.get("/reviews", {roomid: room.id}, function (data) {
                         let modal = $(`
                         <div class="modal fade" tabindex="-1" role="dialog">
-                            <div class="modal-dialog modal-lg" role="document">
+                            <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                     <div class="modal-body">
                                     
